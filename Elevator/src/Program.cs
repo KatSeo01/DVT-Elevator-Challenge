4c9d4c2896790644a307a2bad18e9d7fe813ae2f
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-07 22:52:23 +0200

    Modified Program.cs

--- Elevator/src/Program.cs
+++ Elevator/src/Program.cs
@@ -1,26 +1,29 @@
 using System;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ElevatorChallenge
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // Create a building with 10 floors and 2 elevators
             Building building = new Building(10, new ElevatorFactory());
 
             // Add some people to the building
-            building.AddPerson(1, 5);
-            building.AddPerson(2, 3);
-            building.AddPerson(3, 7);
-            building.AddPerson(4, 2);
-            building.AddPerson(5, 8);
+            building.AddPerson(1, 5, 1);
+            building.AddPerson(2, 3, 2);
+            building.AddPerson(3, 7, 3);
+            building.AddPerson(4, 2, 1);
+            building.AddPerson(5, 8, 2);
 
             // Run the simulation
+            await building.RequestElevatorAsync(1, 9, 1);
+
             while (building.HasPeople())
             {
                 building.Update();
+                await Task.Delay(500); // Introduce delay for better visualization
             }
         }
     }
