20f9aa2abc254d26a563a0921a740c41519a2191
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-08 15:40:38 +0200

    Elevator test case

---
+++ Elevator/tests/ElevatorTests.cs
@@ -0,0 +1,63 @@
+[TestClass]
+public class ElevatorTest
+{
+    [TestMethod]
+    public async Task MoveToFloorAsync_SameFloor()
+    {
+        // Arrange
+        int numFloors = 5;
+        var elevator = new Elevator(numFloors);
+
+        // Act
+        await elevator.MoveToFloorAsync(1);
+
+        // Assert
+        Assert.AreEqual(1, elevator.CurrentFloor);
+    }
+
+    [TestMethod]
+    public async Task MoveToFloorAsync_MovingUp()
+    {
+        // Arrange
+        int numFloors = 5;
+        var elevator = new Elevator(numFloors);
+
+        // Act
+        await elevator.MoveToFloorAsync(3);
+
+        // Assert
+        Assert.AreEqual(3, elevator.CurrentFloor);
+    }
+
+    [TestMethod]
+    public async Task MoveToFloorAsync_MovingDown()
+    {
+        // Arrange
+        int numFloors = 5;
+        var elevator = new Elevator(numFloors);
+        elevator.currentFloor = 5;
+        elevator.direction = -1;
+
+        // Act
+        await elevator.MoveToFloorAsync(3);
+
+        // Assert
+        Assert.AreEqual(3, elevator.CurrentFloor);
+    }
+
+    [TestMethod]
+    public async Task MoveToFloorAsync_RemovesPassengers()
+    {
+        // Arrange
+        int numFloors = 5;
+        var elevator = new Elevator(numFloors);
+        var person = new Person(2);
+        elevator.AddPerson(person);
+
+        // Act
+        await elevator.MoveToFloorAsync(2);
+
+        // Assert
+        Assert.IsFalse(elevator.people.Contains(person));
+    }
+}
