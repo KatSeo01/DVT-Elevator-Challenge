adff38357b970eb41080459dff3141ee4f20ce68
Katlego Seoketsa <33416076+KatSeo01@users.noreply.github.com>
2024-01-09 11:02:22 +0200

    Updated my interface

--- Elevator/src/IElevator.cs
+++ Elevator/src/IElevator.cs
@@ -1,6 +1,11 @@
-//The IElevator interface defines methods: CurrentFloor, and AddPerson
-interface IElevator
+//The IElevator interface defines methods
+interface IElevator : IElevatorMovement
 {
     int CurrentFloor { get; }
+    Direction CurrentDirection { get; }
+    ElevatorState State { get; }
+    List<Person> Passengers { get; }
+    int PassengerLimit { get; set; }
     void AddPerson(Person person);
+    void Update();
 }
